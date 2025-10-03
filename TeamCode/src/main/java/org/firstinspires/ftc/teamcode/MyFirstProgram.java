5dab71bf364d4814c66cf936de0256ab5c99c7d1
spb13n <bala.perumal@yahoo.com>
2025-10-03 16:37:16 -0400

    First samples

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFirstProgram.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class MyFirstProgram extends OpMode {
+    @Override
+    public void init() {
+        telemetry.addData ("FTC", "This year's challenge is Decode");
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TechKnowLogic.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+@TeleOp
+public class TechKnowLogic extends OpMode {
+
+    @Override
+    public void init() {
+        int x = 15000;
+        int y = 4;
+        String teamName = "TechKNOWLogic";
+        int teamNumber = x + y;
+
+//        Telemetry.addData( "Team Name", teamName);
+        telemetry.addData( "Team Name", teamName);
+        telemetry.addData( "Team number", teamNumber);
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
